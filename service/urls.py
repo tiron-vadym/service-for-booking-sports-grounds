ac840ef8a78fabfe2ae8c64b92fad66f19a9915c
Vadym Tiron <tironvadim1@gmail.com>
2024-05-23 10:22:21 +0300

    implement router for PaymentViewSet

--- service/urls.py
+++ service/urls.py
@@ -1,15 +1,19 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from service.views import SportGroundViewSet, ReservationViewSet, FAQView
+from service.views import (
+    SportGroundViewSet,
+    ReservationViewSet,
+    PaymentViewSet
+)
 
 router = routers.DefaultRouter()
 router.register("sports-grounds", SportGroundViewSet)
 router.register("reservations", ReservationViewSet)
+router.register("payments", PaymentViewSet)
 
 urlpatterns = [
-    path("", include(router.urls)),
-    path("about/", FAQView.as_view(), name="about"),
+    path("", include(router.urls))
 ]
 
 app_name = "service"
