a1fd28b1d47b3fbb9a0d51cac7628609c8e32526
Vadym Tiron <tironvadim1@gmail.com>
2024-05-23 10:21:04 +0300

    add new serializer for Payment and serializers for custom actions

--- service/serializers.py
+++ service/serializers.py
@@ -1,51 +1,110 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 
-from service.models import SportGround, Reservation
+from service.models import SportGround, Reservation, Payment
 
 
 class SportGroundSerializer(serializers.ModelSerializer):
     class Meta:
         model = SportGround
         fields = [
+            "id",
             "name",
             "image",
             "location",
             "price",
+            "activity",
+            "phone"
         ]
 
 
 class SportGroundImageSerializer(serializers.ModelSerializer):
     class Meta:
         model = SportGround
-        fields = ["image"]
+        fields = [
+            "id",
+            "image"
+        ]
+
+
+class SportGroundReservationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Reservation
+        fields = [
+            "id",
+            "place",
+            "day",
+            "time",
+            "duration_hours",
+            "created_at",
+            "personal_data"
+        ]
+        read_only_fields = ["place", "personal_data", "created_at"]
+
+
+class SportGroundScheduleSerializer(serializers.ModelSerializer):
+    schedule = serializers.SerializerMethodField()
+
+    class Meta:
+        model = SportGround
+        fields = [
+            "id",
+            "schedule"
+        ]
+
+    def get_schedule(self, obj):
+        return Reservation.objects.filter(place=obj)
 
 
 class ReservationSerializer(serializers.ModelSerializer):
-    place = serializers.SlugRelatedField(slug_field="name", queryset=SportGround.objects.all())
+    place = serializers.SlugRelatedField(
+        slug_field="name",
+        queryset=SportGround.objects.all()
+    )
 
     class Meta:
         model = Reservation
         fields = [
+            "id",
             "place",
-            "personal_data",
-            "created_at",
             "day",
-            "time"
+            "time",
+            "duration_hours",
+            "created_at",
+            "personal_data"
         ]
-        read_only_fields = ["personal_data", "created_at"]
+        read_only_fields = ["place", "personal_data", "created_at"]
 
-    def validate(self, data):
-        existing_reservations = Reservation.objects.filter(day=data["day"], time=data["time"])
-        if self.instance:
-            existing_reservations = existing_reservations.exclude(pk=self.instance.pk)
 
-        if existing_reservations.exists():
-            raise serializers.ValidationError("This combination of day and time is already reserved.")
-
-        return data
+class ScheduleRetrieveSerializer(ReservationSerializer):
+    personal_data = serializers.SlugRelatedField(
+        slug_field="email",
+        queryset=get_user_model().objects.all()
+    )
 
 
 class ReservationListRetrieveSerializer(ReservationSerializer):
     place = SportGroundSerializer(read_only=True)
-    personal_data = serializers.SlugRelatedField(slug_field="email", queryset=get_user_model().objects.all())
+    personal_data = serializers.SlugRelatedField(
+        slug_field="email",
+        queryset=get_user_model().objects.all()
+    )
+
+
+class PaymentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Payment
+        fields = [
+            "id",
+            "status",
+            "reservation",
+            "session_url",
+            "session_id",
+            "money_to_pay",
+        ]
+        read_only_fields = [
+            "status",
+            "session_url",
+            "session_id",
+            "money_to_pay"
+        ]
