55c8ed6eb21e629e7c7afa47f48cfa2fa5bb0173
Vadym Tiron <tironvadim1@gmail.com>
2024-06-30 17:41:14 +0300

    change @action in viewset

--- client/views.py
+++ client/views.py
@@ -5,11 +5,15 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.exceptions import TokenError
 from rest_framework_simplejwt.tokens import RefreshToken
-from dj_rest_auth.serializers import PasswordChangeSerializer
 
-from client.serializers import UserSerializer, MeSerializer
+from client.serializers import (
+    UserSerializer,
+    MeSerializer,
+    PasswordChangeSerializer
+)
 from client.models import User
-from service.serializers import ScheduleRetrieveSerializer
+from service.models import SportsComplex, Booking
+from service.serializers import SportsComplexRetrieveSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -30,7 +34,7 @@ class UserViewSet(viewsets.ModelViewSet):
         elif self.action == "list":
             return UserSerializer
         elif self.action == "schedule":
-            return ScheduleRetrieveSerializer
+            return SportsComplexRetrieveSerializer
         elif self.action == "password":
             return PasswordChangeSerializer
         return self.serializer_class
@@ -45,17 +49,25 @@ class UserViewSet(viewsets.ModelViewSet):
     )
     def schedule(self, request):
         user = self.request.user
-        bookings = user.users.all().select_related()
-        serializer = self.get_serializer(bookings, many=True)
+        bookings = Booking.objects.filter(
+            personal_data=user
+        ).select_related("field__complex")
+        sport_complex_ids = set(
+            booking.field.complex.id for booking in bookings
+        )
+        sport_complexes = SportsComplex.objects.filter(
+            id__in=sport_complex_ids
+        ).prefetch_related("fields__bookings")
+        serializer = self.get_serializer(sport_complexes, many=True)
         return Response(
-            {"schedule": serializer.data},
+            serializer.data,
             status=status.HTTP_200_OK
         )
 
-    @action(detail=True, methods=["PUT", "PATCH"])
+    @action(detail=True, methods=["PUT", "PATCH"], permission_classes=[IsAuthenticated])
     def password(self, request, pk=None):
         user = self.get_object()
-        serializer = self.get_serializer(data=request.data)
+        serializer = self.get_serializer(data=request.data, context={"request": request})
         serializer.is_valid(raise_exception=True)
         user.set_password(serializer.validated_data["new_password"])
         user.save()
